baf331d715fc5b8fcd43933ba642cfb7ae48b9ea
Polina Petrenko <147924269+PolinaPetrenko@users.noreply.github.com>
2025-01-13 18:34:55 +0100

    hw-6

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,15 @@
-export const fetchPhotosByQuery = searchedQuery => {
-    return fetch(`https://pixabay.com/api/?${searchedQuery}`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
+import axios from "axios";
+axios.defaults.baseURL = 'https://pixabay.com';
+export const fetchPhotosByQuery = (searchedQuery, page) => {
+    const requestParams = {
+        q: searchedQuery,
+        page: page,
+        key: "48186759-c0bb4a6385eae122fd1e343f0",
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+        per_page: 15,
+    };
 
-        return response.json();
-    })
+    return axios.get('/api/', { params: requestParams });
 }
